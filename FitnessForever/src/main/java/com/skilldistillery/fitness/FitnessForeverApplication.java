43883bfd901be424728e95a363fa7b49a5ec3e82
Andrew Shumway <shumway.andrew@yahoo.com>
2022-01-07 15:40:37 -0700

    Create Spring Boot Project

---
+++ FitnessForever/src/main/java/com/skilldistillery/fitness/FitnessForeverApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessForeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessForeverApplication.class, args);
+	}
+
+}
